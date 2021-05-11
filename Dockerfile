7ea1454f00d99ddbf7f8705873c11b7949498357
gogopavl <pgogousis@gmail.com>
2021-05-11 22:35:12 +0100

    add config for heroku deployment and adjust dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,14 @@
+FROM maven:3-jdk-11
+
+ADD . /abetta-gateway
+WORKDIR /abetta-gateway
+
+RUN ls -l
+RUN mvn clean install -DskipTests
+
 FROM openjdk:11.0-jre-slim
-ADD target/*.jar app.jar
-ENTRYPOINT java -jar app.jar
+
+VOLUME /tmp
+COPY --from=0 /abetta-gateway/target/abetta-gateway*.jar app.jar
+
+CMD [ "sh", "-c", "java -Dserver.port=$PORT -Xmx300m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8 -XX:+UseContainerSupport -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=heroku -jar /app.jar" ]
--- src/main/java/com/abettaworld/abettagateway/configuration/SecurityConfiguration.java
+++ src/main/java/com/abettaworld/abettagateway/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
@@ -15,7 +16,10 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity.authorizeExchange()
+        serverHttpSecurity
+                .cors()
+                .and()
+                .authorizeExchange()
                 .pathMatchers("/get").permitAll()
                 .pathMatchers("/login", "/abetta-xp/**", "/something").authenticated()
                 .and()
@@ -23,14 +27,40 @@ public class SecurityConfiguration {
         return serverHttpSecurity.build();
     }
 
+//    @Bean
+//    CorsWebFilter corsWebFilter(UriConfiguration uriConfiguration) {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//        corsConfig.setAllowedOrigins(Collections.singletonList(uriConfiguration.getAllowedOrigin()));
+//        corsConfig.addAllowedMethod("PUT");
+//        corsConfig.addAllowedMethod("GET");
+//        corsConfig.addAllowedMethod("POST");
+//        corsConfig.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
     @Bean
-    CorsWebFilter corsWebFilter(UriConfiguration uriConfiguration) {
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Collections.singletonList(uriConfiguration.getAllowedOrigin()));
-        corsConfig.addAllowedMethod("PUT");
-        corsConfig.addAllowedMethod("GET");
-        corsConfig.addAllowedMethod("POST");
         corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
